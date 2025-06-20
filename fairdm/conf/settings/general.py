299c49318e1368ec1abd65571ca1ea5030f35eb7
Sam <samuel.scott.jennings@gmail.com>
2025-06-20 18:02:28 +0200

    add compress to setup tools

--- fairdm/conf/settings/general.py
+++ fairdm/conf/settings/general.py
@@ -141,6 +141,7 @@ DJANGO_SETUP_TOOLS = {
         "always_run": [
             ("migrate", "--no-input"),
             ("collectstatic", "--noinput"),
+            ("compress",),
             "django_setup_tools.scripts.sync_site_id",
         ],
     },
@@ -155,9 +156,6 @@ DJANGO_SETUP_TOOLS = {
     },
     "production": {
         "merge": True,  # merge with the default commands
-        "on_initial": [
-            ("loaddata", "myapp"),
-        ],
         "always_run": [
             ("compress",),
         ],
