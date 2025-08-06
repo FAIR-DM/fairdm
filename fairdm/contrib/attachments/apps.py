91970c211907f313b25af405b654d6bde25a7695
Sam <samuel.scott.jennings@gmail.com>
2025-08-06 11:41:16 +0200

    remove obsolete contrib app

--- fairdm/contrib/attachments/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-from django.utils.translation import gettext_lazy as _
-
-
-class FairDMAttchmentsConfig(AppConfig):
-    name = "fairdm.contrib.attachments"
-    label = "fairdm_attachments"
-    verbose_name = _("Attachments")
