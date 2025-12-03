285bf711e74003beba0adf4d4a6be912a60f73a9
Sam <samuel.scott.jennings@gmail.com>
2025-12-03 09:12:42 +0100

    refactored view from utils to activity_stream

--- fairdm/contrib/activity_stream/urls.py
+++ fairdm/contrib/activity_stream/urls.py
@@ -1,8 +1,9 @@
 from django.urls import include, path
 
-app_name = "activity_stream"
+from .views import follow_unfollow
 
 urlpatterns = [
     # Include django-activity-stream's built-in URLs
     path("", include("actstream.urls")),
+    path("activity/follow-object/<str:uuid>", follow_unfollow, name="follow-object"),
 ]
