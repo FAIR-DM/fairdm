e14f2ee8ce0699e7de662e738be4e423e4cfe8f5
Sam <samuel.scott.jennings@gmail.com>
2025-12-03 09:25:52 +0100

    remove image cropping widget

--- fairdm/forms/fields.py
+++ fairdm/forms/fields.py
@@ -1,42 +1,7 @@
 import datetime
-import json
 
-from client_side_image_cropping import ClientsideCroppingWidget
 from django import forms
 from partial_date import PartialDate
-
-
-class ImageCroppingWidget(ClientsideCroppingWidget):
-    def __init__(self, width: int, height: int, config, result, empty_text=None, *args, **kwargs):
-        self.config = config
-        self.result = result
-        self.empty_text = empty_text
-        super().__init__(width, height, width, height, *args, **kwargs)
-
-    def get_context(self, name, value, attrs):
-        context = super().get_context(name, value, attrs)
-        context["widget"].update(
-            {
-                "config": json.dumps(self.config),
-                "result": json.dumps(self.result),
-                "empty_text": self.empty_text,
-            }
-        )
-        return context
-
-    class Media:
-        css = {
-            "all": (
-                "client_side_image_cropping/croppie.css",
-                # "client_side_image_cropping/cropping_widget.css",
-                # "cropping_widget/cropping.css",
-            ),
-        }
-        js = (
-            "client_side_image_cropping/croppie.min.js",
-            # "cropping_widget/cropping.js",
-            # "client_side_image_cropping/cropping_widget.js",
-        )
 
 
 class PartialDateWidget(forms.SelectDateWidget):
