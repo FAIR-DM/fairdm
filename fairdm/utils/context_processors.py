6424e86dbec5b190d5a9a9669682e90f0adeeaa6
Sam <samuel.scott.jennings@gmail.com>
2025-12-03 09:27:45 +0100

    add registry to context processor

--- fairdm/utils/context_processors.py
+++ fairdm/utils/context_processors.py
@@ -3,24 +3,28 @@ import json
 from django.conf import settings
 
 from fairdm.contrib.identity.models import Authority, Database
+from fairdm.registry import registry
 
 
 def fairdm(request):
     # Get the current site
     # current_site = Site.objects.get_current()
     """A context processor that adds the following variables to the context:"""
+
     context = {
         "config": {
             "site_name": settings.SITE_NAME,
             "site_domain": settings.SITE_DOMAIN,
             "allow_registration": settings.ACCOUNT_ALLOW_REGISTRATION,
+            "portal_description": getattr(settings, "PORTAL_DESCRIPTION", None),
             # "site_domain": current_site.domain,
         },
         "identity": {
-            "database": Database.get_solo(),
+            "dataset": Database.get_solo(),
             "authority": Authority.get_solo(),
         },
         "theme_options": settings.FAIRDM_CONFIG,
+        "registry": registry,
     }
     context["json_config"] = json.dumps(context["config"])
     return context
