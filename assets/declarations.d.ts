3658c7d684fdabde30bd80e457c610bcadfc3866
Sam <samuel.scott.jennings@gmail.com>
2026-06-19 17:26:01 +0200

    cleanup assets and static

--- assets/declarations.d.ts
+++
@@ -1,5 +0,0 @@
-declare module 'justgage'
-declare module '@citation-js/core'
-declare module 'citation-js'
-declare module 'citation-js/plugin-csl'
-declare module 'swagger-client'
--- assets/src/htmx.js
+++
@@ -1,3 +0,0 @@
-import "htmx.org"
-import htmx from "htmx.org"
-window.htmx = htmx
